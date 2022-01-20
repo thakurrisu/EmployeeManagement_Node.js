623674b846f7325c35543fecbc7f9ba64e4495ff
Rishikesh <rishikesh.kumar.921677@gmail.com>
2022-01-20 20:24:25 +0530

    Online Database connection

--- src/App.js
+++ src/App.js
@@ -1,78 +1,70 @@
-
-const express=require('express');
+const express = require('express');
 const { clearCookie } = require('express/lib/response');
 
-const path=require('path');
+const path = require('path');
 const { off } = require('process');
 
 //db connection
-require(path.join(__dirname,('db/conn.js')))
-const Employee=require(path.join(__dirname,'/models/Employee'))
+require(path.join(__dirname, 'db/conn.js'));
+const Employee = require(path.join(__dirname, '/models/Employee'));
 
-
-var app=express();
-const port= 3000
+var app = express();
+const port = 3000;
 
 app.use(express.json());
-//Read
-app.get("/student",async(req,res)=>{
-    try{
-        const data=await Employee.find()
-        res.status(201).send(data)
-    }
-  catch(err){
+//Read studen function
+const getStudents = async (req, res) => {
+  try {
+    const data = await Employee.find();
+    res.status(201).send(data);
+  } catch (err) {
     res.status(400).send(err);
     console.log(err);
   }
-})
-
-app.get("/student/:id",async(req,res)=>{
-    try{
-        const id =req.params.id
-        const data=await Employee.findById( { _id : id} )
-        res.status(201).send(data)
-    }
-  catch(err){
+};
+const getStudentsbyId = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Employee.findById({ _id: id });
+    res.status(201).send(data);
+  } catch (err) {
     res.status(400).send(err);
     console.log(err);
   }
-})
+};
+const createStudent = (req, res) => {
+  const data = new Employee(req.body);
 
-//create
-app.post("/student",(req,res)=>{
-    const data=new Employee(req.body)
-
-    data.save().then(()=>{
-        res.status(201).send(data)
-     
-    }).catch((err)=>{
-        res.status(400).send(err);
-        console.log(err);
+  data
+    .save()
+    .then(() => {
+      res.status(201).send(data);
     })
-   
-})
-
-//detele
-app.delete("/student/:id",async(req,res)=>{
-  try{
-    const _id=req.params.id
-  console.log(_id)
-  const deleteStudent = await Employee.findByIdAndDelete(_id)
-        res.status(200).send(deleteStudent)
-  }catch(err){
+    .catch((err) => {
       res.status(400).send(err);
+      console.log(err);
+    });
+};
+const deleteStudent = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    console.log(_id);
+    const deleteStudent = await Employee.findByIdAndDelete(_id);
+    res.status(200).send(deleteStudent);
+  } catch (err) {
+    res.status(400).send(err);
   }
-})
+};
+/*
+app.get("/student",getStudents)
+app.post("/student",createStudent)
 
+app.get('/student/:id', getStudentsbyId);
+app.delete('/student/:id', deleteStudent);
+*/
+app.route('/student').get(getStudents).post(createStudent);
+app.route('/student/:id').get(getStudentsbyId).delete(deleteStudent);
 
-
-
-
-
-
-
-
-app.listen(port,()=>{
-    console.log(`working on ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`working on ${port}`);
+});
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,11 @@
-const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/employees_db").then(()=>{
-    console.log("Db connection successful")
-}).catch((err)=>{
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://thakurrisu:1234@cluster0.zbdra.mongodb.net/employees_db?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Db connection successful');
+  })
+  .catch((err) => {
     console.log(err);
-
-})
+  });
