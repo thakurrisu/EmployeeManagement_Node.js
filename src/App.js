96b1553a85e366e3ac7bfebed879812530579080
Rishikesh <rishikesh.kumar.921677@gmail.com>
2022-01-20 12:56:47 +0530

    -added delelte feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 const express=require('express');
 const { clearCookie } = require('express/lib/response');
 
-const path=require('path')
+const path=require('path');
+const { off } = require('process');
 
 //db connection
 require(path.join(__dirname,('db/conn.js')))
-const Employee=require(path.join(__dirname,'/models/Student'))
+const Employee=require(path.join(__dirname,'/models/Employee'))
 
 
 var app=express();
@@ -17,13 +18,26 @@ app.use(express.json());
 app.get("/student",async(req,res)=>{
     try{
         const data=await Employee.find()
-        res.status(200).send(data)
+        res.status(201).send(data)
     }
   catch(err){
     res.status(400).send(err);
     console.log(err);
   }
 })
+
+app.get("/student/:id",async(req,res)=>{
+    try{
+        const id =req.params.id
+        const data=await Employee.findById( { _id : id} )
+        res.status(201).send(data)
+    }
+  catch(err){
+    res.status(400).send(err);
+    console.log(err);
+  }
+})
+
 //create
 app.post("/student",(req,res)=>{
     const data=new Employee(req.body)
@@ -38,7 +52,17 @@ app.post("/student",(req,res)=>{
    
 })
 
-
+//detele
+app.delete("/student/:id",async(req,res)=>{
+  try{
+    const _id=req.params.id
+  console.log(_id)
+  const deleteStudent = await Employee.findByIdAndDelete(_id)
+        res.status(200).send(deleteStudent)
+  }catch(err){
+      res.status(400).send(err);
+  }
+})
 
 
 
